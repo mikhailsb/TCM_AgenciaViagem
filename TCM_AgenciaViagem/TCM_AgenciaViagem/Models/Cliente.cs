669f1e1d9eac74568ad8345172bfc715fe46a8a6
Mikhail Bispo <anaomsb@hotmail.com>
2020-04-22 22:14:22 -0300

    Criado a classe Cliente
    Criado a classe Cliente.

---
+++ TCM_AgenciaViagem/TCM_AgenciaViagem/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCM_AgenciaViagem.Models
+{
+    public class Cliente
+    {
+        public ushort IdCliente { get; set; }
+        public string NomeCliente { get; set; }
+        public string Email { get; set; }
+        public string SenhaCliente { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Telefone { get; set; }
+     
+        //-----------------------
+        //CadastrarCliente()
+        //ConsultaAgenda()
+        //RealizarCompra()
+    }
+}
