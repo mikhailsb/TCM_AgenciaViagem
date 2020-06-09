628c3bdcf6ac2cd6f88b3ec1838e1655ba39d961
Mikhail Bispo <anaomsb@hotmail.com>
2020-06-09 18:59:31 -0300

    Ajustando o Layout
    Corrigindo o layout.

--- TCM_AgenciaViagem/TCM_AgenciaViagem/Models/Funcionario.cs
+++ TCM_AgenciaViagem/TCM_AgenciaViagem/Models/Funcionario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,15 @@ namespace TCM_AgenciaViagem.Models
     {
         public ushort IdFuncionario { get; set; }
 
-
+        [Required(ErrorMessage = "Nome do funcionário é o brigatório.")]
         public string NomeFuncioario { get; set; }
+
+        [Required(ErrorMessage = "Login do funcionário é obrigatório.")]
+        [StringLength(50, MinimumLength=5, ErrorMessage ="O campo deve conter mais de 5 caracteres.")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Senha é um campo obrigatório.")]
+        [StringLength(16, MinimumLength = 8, ErrorMessage ="A senha deve ter de 8 a 16 caracteres.")]
         public string SenhaFuncionario { get; set; }
 
         public void CadastrarFuncionario() 
