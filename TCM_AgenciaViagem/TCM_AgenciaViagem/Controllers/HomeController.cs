449fa4202227267f1544f6016a8c547b7543c06c
Mikhail Bispo <anaomsb@hotmail.com>
2020-07-08 11:20:59 -0300

    Divisao da aplicao
    Divisão da aplição utilizando a metodologia DAO.
    Foi criado as aplicações AppBanco, AppClasses, AppDAO.
    Cadastro de funcionários já ocorre.

--- TCM_AgenciaViagem/TCM_AgenciaViagem/Controllers/HomeController.cs
+++ TCM_AgenciaViagem/TCM_AgenciaViagem/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace TCM_AgenciaViagem.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Logado()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
